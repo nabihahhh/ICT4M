b4b7ac77c17805f4c1397de3742d8c71f030ea71
AmirahMasri <50224073+AmirahMasri@users.noreply.github.com>
2019-12-22 18:21:49 +0800

    Miraupdate
    Update speakers js

--- js/speakers.js
+++ js/speakers.js
@@ -1,10 +1,72 @@
-$('.element').each(function() {
-    $(this).mouseover(function() {
-        $(this).addClass('active');
-      $('.stage').children('.element').not('.active').addClass('inactive');
-    });
-    $(this).mouseleave(function() {
-        $(this).removeClass('active');
-        $('.stage').children('.element').not('.active').removeClass('inactive');
-    });
-});
+var modal1 = document.getElementById("myModal1");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn1");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close1")[0];
+
+btn.onclick = function() {
+  modal1.style.display = "block";
+}
+
+span.onclick = function() {
+  modal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+
+
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn2");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close2")[0];
+
+btn.onclick = function() {
+  modal2.style.display = "block";
+}
+
+span.onclick = function() {
+  modal2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal3.style.display = "none";
+  }
+}
+
+var modal3 = document.getElementById("myModal3");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn3");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close3")[0];
+
+btn.onclick = function() {
+  modal3.style.display = "block";
+}
+
+span.onclick = function() {
+  modal3.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+}
